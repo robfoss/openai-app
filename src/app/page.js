a689096e42af3f7c479c196fb7ff651b28d0e4af
robfoss <robfoss@rocketmail.com>
2023-04-06 13:16:35 -0300

    rearranged folder structure for auth0

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import { Inter } from 'next/font/google';
-
+import Link from 'next/link';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>This is the Home Page</h1>
+      <div>
+        <Link href={'/api/auth/login'}>Login</Link>
+      </div>
+    </main>
+  );
 }
---
+++ src/app/tokens/token-topup/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TokenTopUpPage = () => {
+  return <div>TokenTopUpPage</div>;
+};
+
+export default TokenTopUpPage;
