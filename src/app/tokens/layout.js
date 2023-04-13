401ade6430b0a2426d1be241e6bf52422cdf49f4
robfoss <robfoss@rocketmail.com>
2023-04-13 12:39:32 -0400

    added token handler function

--- src/app/tokens/layout.js
+++ src/app/tokens/layout.js
@@ -3,22 +3,50 @@ import '../globals.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import { GiTwoCoins } from 'react-icons/gi';
 import Providers from '../providers';
 
 import Logo from '@/components/Logo';
 
-export default function PostLayout({ children }) {
+import { Inconsolata, IBM_Plex_Mono } from 'next/font/google';
+
+const inconsolata = Inconsolata({
+  variable: '--font-inconsolata',
+  weight: ['200', '300', '400', '500'],
+  subsets: ['latin'],
+});
+const ibmPlexMono = IBM_Plex_Mono({
+  variable: '--font-ibm-plex-mono',
+  weight: ['200', '300', '400', '500'],
+  subsets: ['latin'],
+});
+
+export default function TokenLayout({ children }) {
   const { user } = useUser();
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      className={`${inconsolata.variable}, ${ibmPlexMono.variable}`}
+    >
       <body>
         <Providers>
           <div className='grid grid-cols-[300px_1fr] h-screen max-h-screen'>
             <div className='flex flex-col text-white overflow-hidden'>
-              <div className='bg-slate-700'>
+              <div className='bg-slate-700 px-2'>
                 <Logo />
-                <Link href={'/post/new'}>New Post</Link>
-                <Link href={'/tokens/token-topup'}>0 tokens available</Link>
+                <Link
+                  className='bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer px-4  py-2 rounded-md hover:bg-green-600 transition-colors block'
+                  href={'/post/new'}
+                >
+                  New Post
+                </Link>
+                <Link
+                  className='mt-2 text-center flex'
+                  href={'/tokens/token-topup'}
+                >
+                  <GiTwoCoins className='text-yellow-500 w-6 h-6' />
+                  <span className='pl-1'>0 tokens available</span>
+                </Link>
               </div>
               <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-700 to-cyan-700'>
                 list of posts
@@ -47,7 +75,7 @@ export default function PostLayout({ children }) {
                 )}
               </div>
             </div>
-            <div className='bg-green-500'>{children}</div>
+            <div className=''>{children}</div>
           </div>
         </Providers>
       </body>
--- src/app/tokens/token-topup/page.jsx
+++ src/app/tokens/token-topup/page.jsx
@@ -3,5 +3,20 @@ import React from 'react';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
 export default withPageAuthRequired(function TokenTopUp() {
-  return <div>Token Top Up</div>;
+  const handleClick = async () => {
+    await fetch(`/api/addTokens`, {
+      method: 'POST',
+    });
+  };
+  return (
+    <div>
+      <h1>Token Top Up</h1>
+      <button
+        className='bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer px-4  py-2 rounded-md hover:bg-green-600 transition-colors block'
+        onClick={handleClick}
+      >
+        Add Tokens
+      </button>
+    </div>
+  );
 });
